7ced7415b49471c91d500013e28172dcb89509b7
chs_0303 <hsoorang@naver.com>
2021-09-30 14:12:07 +0900

    survey view&unit test - Add: survey created - fix : survey&product url - Add : userpage-survey result list - Add : userpage-survey result list

--- surveys/urls.py
+++ surveys/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
 
-from surveys.views import SurveyView, SymptomView, SurveyResultView
+from surveys.views import SurveyView, SymptomView, SurveyResultView, UserSurveyListView, UserSurveyResultView
 
 urlpatterns = [
     path('/survey', SurveyView.as_view()),
     path('/symptom', SymptomView.as_view()),
-    path('/result', SurveyResultView.as_view())
+    path('/result', SurveyResultView.as_view()),
+    path('/resultlist', UserSurveyListView.as_view()),
+    path('/user-result', UserSurveyResultView.as_view())
 ]
